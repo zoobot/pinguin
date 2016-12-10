86013ceabb1b21596abad903afe71fa2bdff999e
botzoo <rommims@gmail.com>
2016-12-10 14:59:30 -0800

    started feed

--- client/src/pages/feed.jsx
+++ client/src/pages/feed.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
 
+var feed = {urls: ['http://www.evilmadscientist.com/feed/','http://boingboing.net/feed', 'http://hackaday.com/feed/']}
+
 class Feed extends React.Component {
+
+  handFeeds(feedArr){
+    const textarea = document.getElementById('feeds')
+    feedArr.map(function(url) {
+      feednami.load(url)
+        .then(feed => {
+          console.log(feed)
+          textarea.value = ''
+          for(let entry of feed.entries) {
+            textarea += `${entry.title}\n${entry.link}\n\n`
+          }
+          console.log(textarea)
+        })
+    })
+  }
+
+  handleGet(event) {
+    Util.getFeed()
+  }
+
   render() {
     return (
-      <h1>FEED</h1>
+      <form onSubmit={this.handleFeeds}>
+        <button type="submit" value="Submit">REFRESH</button>
+      </form>
+      <div id="feeds"></div>
     )
   }
 }
