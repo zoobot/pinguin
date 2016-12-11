8c9c1fc6f59d643632e8617d847b7567e44c2a29
botzoo <rommims@gmail.com>
2016-12-10 22:45:34 -0800

    more tries at getting promise to render in react

--- client/src/pages/feed.jsx
+++ client/src/pages/feed.jsx
@@ -15,26 +15,38 @@ class Feed extends React.Component {
   }
 
   handleFetchFeed(event) {
-    console.log("handleChangeFeed triggered")
+    // console.log("handleChangeFeed triggered")
     var textarea = document.getElementById('feed')
     var list = ''
-    list = subscriptions.urls.map(function(url) {
-        feednami.load(url)
-          .then(feed => {
-            for(let entry of feed.entries) {
-              // list += `${entry.title}\n${entry.link}\n\n`
-              // textarea.value += `${entry.title}\n${entry.link}\n\n`
-              list.push(entry.title)
-              list.push(entry.link)
-              console.log('handleFeed list',list)
-            }
-
-          })
+    subscriptions.urls.map(function(url) {
+        console.log(feednami.load(url))
+          // .then(feed => {
+          //   console.log(feed)
+          //   for(let entry of feed.entries) {
+          //     list = ''
+          //     list += `${entry.title}\n${entry.link}\n\n`
+          //     // textarea.value += `${entry.title}\n${entry.link}\n\n`
+          //     // list.push(entry.title)
+          //     // list.push(entry.link)
+          //   }
+          // console.log('handleFeed list',textarea)
+          // })
+          // .then(function(data) {
+          //   // return data.json()
+          //   console.log(data)
+          //   // this.setState({defaultFeed: data.entries})
+          //   // console.log(this.state.defaultFeed)
+          // })
+          // .bind(this))
       })
 
-    return list
-    // this.setState({defaultFeed: list})
+    // return listfeed
+
     event.preventDefault();
+  }
+
+  componentDidMount(query) {
+    this.handleFetchFeed();
   }
 
 
@@ -46,38 +58,36 @@ class Feed extends React.Component {
           feednami.load(url)
             .then(feed => {
               for(let entry of feed.entries) {
-                textarea.value += `${entry.title}\n${entry.link}\n\n`
+                // list += `${entry.title}\n${entry.link}\n\n`
                   var tempArr = []
-                  List.push(`${entry.title}\n${entry.link}\n\n`)
+                  // List.push(`${entry.title}\n${entry.link}\n\n`)
                 // return (
                   // <a href={entry.title}>{entry.title}</a>,
                   // <a href={entry.link}>{entry.link}</a>,
-                  // List.push(`${entry.title}\n${entry.link}\n\n`)
+                  List.push(`${entry.title}\n${entry.link}\n\n`)
                   // List.push(`${entry.title}\n${entry.link}\n\n`)
                 // )
               }
-              // return List
+              return List
             })
           })
-    console.log('theList', List)
+    console.log('theList', theList)
     return (
       <div>
         <h1>{ title }</h1>
         <button onClick={this.handleFetchFeed}>REFRESH</button>
         <div id="feed">
             <ul>
-              {subscriptions.urls.map(function(url) {
-                feednami.load(url)
-                .then(feed => {
-                  for(let entry of feed.entries) {
-                    return (
-                      <li key={ entry.title }>{entry.title}</li>,
-                      <li key={ entry.link }>{entry.title}</li>
-                    )
-                  }
-                })
-              })}
+                {subscriptions.urls.map(function(url){
+                    feednami.load(url)
+                    .then(feed => {
+                      for(let entry of feed.entries) {
+                        return <li key={ entry }>{entry.title}</li>
+                      }
+                    })
+                  })}
             </ul>
+
         </div>
       </div>
     )
