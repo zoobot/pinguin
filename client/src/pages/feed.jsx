5b2f50d0eaef9eff8cf8e16a433c6c2d52f41f89
botzoo <rommims@gmail.com>
2016-12-10 21:34:36 -0800

    stuck on rendering, not getting anywhere

--- client/src/pages/feed.jsx
+++ client/src/pages/feed.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Router, Route, Link } from 'react-router'
 
     var subscriptions = {urls: ['http://www.evilmadscientist.com/feed/','http://boingboing.net/feed', 'http://hackaday.com/feed/']}
 
@@ -13,46 +14,71 @@ class Feed extends React.Component {
     // this.handleGet = this.handleFeeds.bind(this);
   }
 
-  changeSubscription(video) {
-    this.setState({defaultFeed: this.state.defaultFeed});
-  }
-
-  handFeeds(event){
-
-    subscriptions.urls.map(function(url) {
-      feednami.load(url)
-        .then(feed => {
-          for(let entry of feed.entries) {
-            this.state.defaultFeed += `${entry.title}\n${entry.link}\n\n`
-          }
-        })
-    })
-    event.preventDefault();
-  }
-
-  handleChangeFeed(event) {
+  handleFetchFeed(event) {
     console.log("handleChangeFeed triggered")
+    var textarea = document.getElementById('feed')
     var list = ''
-    subscriptions.urls.map(function(url) {
+    list = subscriptions.urls.map(function(url) {
         feednami.load(url)
           .then(feed => {
             for(let entry of feed.entries) {
-              list += `${entry.title}\n${entry.link}\n\n`
+              // list += `${entry.title}\n${entry.link}\n\n`
+              // textarea.value += `${entry.title}\n${entry.link}\n\n`
+              list.push(entry.title)
+              list.push(entry.link)
+              console.log('handleFeed list',list)
             }
-            console.log(list)
+
           })
       })
-    this.setState({defaultFeed: list})
+
+    return list
+    // this.setState({defaultFeed: list})
     event.preventDefault();
   }
 
 
   render() {
+    const { title } = this.props;
+    var List = []
+    var textarea = document.getElementById('feed')
+    var theList = subscriptions.urls.map(function(url) {
+          feednami.load(url)
+            .then(feed => {
+              for(let entry of feed.entries) {
+                textarea.value += `${entry.title}\n${entry.link}\n\n`
+                  var tempArr = []
+                  List.push(`${entry.title}\n${entry.link}\n\n`)
+                // return (
+                  // <a href={entry.title}>{entry.title}</a>,
+                  // <a href={entry.link}>{entry.link}</a>,
+                  // List.push(`${entry.title}\n${entry.link}\n\n`)
+                  // List.push(`${entry.title}\n${entry.link}\n\n`)
+                // )
+              }
+              // return List
+            })
+          })
+    console.log('theList', List)
     return (
       <div>
-        <h1>SIGNIN</h1>
-        <button onClick={this.handleChangeFeed}>REFRESH</button>
-        <div id="feed">{this.state.defaultFeed}</div>
+        <h1>{ title }</h1>
+        <button onClick={this.handleFetchFeed}>REFRESH</button>
+        <div id="feed">
+            <ul>
+              {subscriptions.urls.map(function(url) {
+                feednami.load(url)
+                .then(feed => {
+                  for(let entry of feed.entries) {
+                    return (
+                      <li key={ entry.title }>{entry.title}</li>,
+                      <li key={ entry.link }>{entry.title}</li>
+                    )
+                  }
+                })
+              })}
+            </ul>
+        </div>
       </div>
     )
   }
